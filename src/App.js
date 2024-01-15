4d9075f53a3a4217db72d3817308727b2226937b
Randol Gonzalez <56413028+randol696@users.noreply.github.com>
2024-01-15 13:22:48 -0500

    Added new features- done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,89 @@
 import "./App.css";
 import React from "react";
 import 'bootstrap/dist/css/bootstrap.css'
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Button from "react-bootstrap/Button" ;
+
+
+let famousQuotes = [
+
+  {
+
+    quote: "The only thing we have to fear is fear itself.",
+
+    author: "Franklin D. Roosevelt",
+
+    color: "#FDFD96"
+
+  },
+
+  {
+
+    quote: "I've failed over and over and over again in my life that is why I succeed.",
+
+    author: "Michael Jordan",
+
+    color: "#F08080"
+
+  },
+
+  {
+
+    quote: "In the middle of difficulty lies opportunity.",
+
+    author: "Albert Einstein",
+
+    color: "#ADD8E6"
+
+  },
+
+  {
+
+    quote: "It does not matter how slowly you go as long as you do not stop.",
+
+    author: "Confucius",
+
+    color: "#98FB98"
+
+  },
+
+  {
+
+    quote: "Life is what happens when you’re busy making other plans.",
+
+    author: "John Lennon",
+
+    color: "#DEB887"
+
+  },
+
+  {
+
+    quote: "Love the life you live. Live the life you love.",
+
+    author: "Bob Marley",
+
+    color: "#7B68EE"
+
+  },
+
+  {
+
+    quote: "To be or not to be, that is the question.",
+
+    author: "William Shakespeare",
+
+    color: "#DB7093"
+
+  }
+
+];
+
+const styles={
+  backgroundcolor: 'blue',
+  color:'',
+  fontSize: 40,
+  
+};
 
 class App extends React.Component {
   constructor(props) {
@@ -10,41 +91,39 @@ class App extends React.Component {
     this.state = {
       advice: "",
       author: "",
+      color:"",
     };
   }
 
-  fetchAdvice = () => {
-    fetch("https://api.adviceslip.com/advice")
-      .then((response) => response.json())
-      .then((data) => {
-        // Assuming the API response has a structure like { slip: { advice: "example" } }
-        this.setState({
-          advice: data.slip.advice,
-        });
-        this.setState({
-          author: data.slip.id,
-        });
-      })
-      .catch((error) => {
-        console.error("Error fetching advice:", error);
-      });
+  quoteRandomizer = () => {
+    const randomIndex =Math.floor(Math.random() * famousQuotes.length);
+    const randomQuote = famousQuotes[randomIndex];
+    this.setState({ advice: randomQuote.quote, author: randomQuote.author, color:randomQuote.color});
   };
+
+  componentDidMount() {
+    // Call the quoteRandomizer function when the component is mounted
+    this.quoteRandomizer();
+  }
+
+ 
   render() {
+    
     return (
-      <div>
-      
-        <div className="Card-Container">
-        <div className="" id="quote-box">
+      <div class="container" style={{...styles, color: this.state.color}}>
+        <style>{`body { background-color: ${this.state.color}; }`}</style>
+        <h1>Random quote Machine</h1>
+
+        <div className="card" id="quote-box">
           <p id="text">{this.state.advice}</p>
           <p>Author:</p>
           <p id="author">{this.state.author}</p>
-          <Button id="new-quote" onClick={this.fetchAdvice} variant="primary">
+          <Button id="new-quote" onClick={this.quoteRandomizer} color="{this.state.color}">
             New Quote
           </Button>
-          <a id="tweet-quote" href="_blank" target="_blank">
+          <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">
             <i className="bi bi-twitter-x"></i>Tweet
-          </a>
-        </div>
+          </a> 
         </div>
       </div>
     );
